febab2ffb484689e4412a98a154b8850a1e70500
mtoiya <mtoiya@gmail.com>
2013-04-24 09:31:37 +0900

    emacs/init.elを修正。 タブ幅の設定。

--- emacs/init.el
+++ emacs/init.el
@@ -3,6 +3,8 @@
 
 ;; タブ幅を設定します。
 (setq-default tab-width 4)
+(setq default-tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
 ;; サーバーモードをスタートします。
 (server-start)
