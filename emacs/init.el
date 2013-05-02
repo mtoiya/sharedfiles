87f1ad935c610e7ef957530e730788cd8faded81
mtoiya <mtoiya@gmail.com>
2013-05-02 23:35:04 +0900

    カテゴリ毎に別ファイル化していたEmacsの設定をinit.elにまとめた。

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,124 @@
-;(load-file "~/sharedfiles/emacs/init-package.el")
-;(load-file "~/sharedfiles/emacs/init-helm.el")
-;(load-file "~/sharedfiles/emacs/init-auto-complete.el")
-;(load-file "~/sharedfiles/emacs/init-ffap.el")
-;(load-file "~/sharedfiles/emacs/init-other.el")
-;(load-file "~/sharedfiles/emacs/init-unix-only.el")
-;(load-file "~/sharedfiles/emacs/init-mac-only.el")
+;;;; package
+
+;; パッケージ取得先URLを追加します。
+;(require 'package)
+;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(package-initialize)
+
+
+
+
+
+;;;; helm
+
+;; helm-miniのキー
+;(global-set-key (kbd "C-c h") 'helm-mini)
+
+;; helm-modeの設定
+;(helm-mode 1)
+
+
+
+
+
+;;;; auto-complete
+
+;(require 'auto-complete)
+;(require 'auto-complete-config)
+;(global-auto-complete-mode t)
+
+
+
+
+
+;;;; ffap
+
+;(ffap-bindings)
+
+
+
+
+
+;;;; gtags
+
+;(require 'gtags)
+;(add-hook 'c-mode-common-hook 'gtags-mode)
+;(add-hook 'c++-mode-hook 'gtags-mode)
+
+
+
+
+
+;;;; Unix専用の設定
+
+;; Shellモードの時にzshを使う。
+;(setq shell-file-name "/bin/zsh")
+
+
+
+
+
+;;;; Mac特有の設定
+
+;(setq ns-command-modifier (quote meta))
+;(setq ns-alternate-modifier (quote super))
+
+
+
+
+
+;;;; org-remember
+
+;(require 'org)
+;(org-remember-insinuate)
+;; メモを格納するorgファイルの設定
+;(setq org-directory "~/Dropbox/Documents/")
+;(setq org-default-notes-file (expand-file-name "memo.org" org-directory))
+;; テンプレートの設定
+;(setq org-remember-templates
+;      '(("Note" ?n "** %?\n   %T" nil "Inbox")
+;        ("Todo" ?t "** TODO %?\n   %T" nil "Inbox")))
+
+
+
+
+
+;;;; その他の設定
+
+;;; タブ幅を設定します
+;; デフォルトのタブ幅
+;(setq-default tab-width 4)
+;; TABキー押下時にタブの代わりにスペースを挿入する
+;(setq-default indent-tabs-mode nil)
+;; TABキー押下時にスペース２個を挿入する
+;(setq-default tab-stop-list (number-sequence 2 120 2))
+;; CやC++モードのデフォルトインデントレベル
+;(setq-default c-basic-offset 4)
+;; C/C++/Objective-C/Java等のタブ・インデント設定
+;(add-hook 'c-mode-common-hook '(lambda () (setq tab-stop-list (number-sequence 4 120 4) indent-tabs-mode t)))
+
+;;; 行番号・桁番号を表示する
+;(line-number-mode 1)
+;(column-number-mode 1)
+
+;;; ツールバーとスクロールバーを消す
+;(tool-bar-mode -1)
+;(scroll-bar-mode -1)
+
+;;; シェルに合わせるため、C-hは後退に割り当てる
+;;; ヘルプは<f1>も使えるので
+;(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;;; yesと入力するのは面倒なのでyで十分
+;(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; ファイル内のカーソル位置を記憶する
+;(setq-default save-place t)
+;(require 'saveplace)
+
+;;; サーバーモードをスタートします
+;(server-start)
 
 ;;; テーマを設定する
 ;(load-theme 'whiteboard t)
