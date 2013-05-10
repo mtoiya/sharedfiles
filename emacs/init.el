b5ba10fff442a3be04d2595f6b7be15b7c06b516
mtoiya <mtoiya@gmail.com>
2013-05-10 19:10:35 +0900

    emacs/init.el変更。 migemo-coding-systemの設定。

--- emacs/init.el
+++ emacs/init.el
@@ -117,6 +117,7 @@
 (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8)
 
 
 
