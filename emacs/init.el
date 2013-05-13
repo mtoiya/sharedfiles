230bbff308ac9c14d0ea86de61a9779a2e11b15b
mtoiya <mtoiya@gmail.com>
2013-05-13 22:51:04 +0900

    emacs/init.el変更。 フォントの設定を見なおした。

--- emacs/init.el
+++ emacs/init.el
@@ -227,6 +227,32 @@
 ;;; 他のウィンドウへ移動するコマンドの実行を楽にする
 (global-set-key (kbd "C-t") 'other-window)
 
+;;; フォント設定（http://sakito.jp/emacs/emacs23.htmlから）
+(when (>= emacs-major-version 23)
+ (set-face-attribute 'default nil
+                     :family "monaco"
+                     :height 120)
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'japanese-jisx0208
+  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'japanese-jisx0212
+  '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+ (set-fontset-font
+  (frame-parameter nil 'font)
+  'mule-unicode-0100-24ff
+  '("monaco" . "iso10646-1"))
+ (setq face-font-rescale-alist
+      '(("^-apple-hiragino.*" . 1.2)
+        (".*osaka-bold.*" . 1.2)
+        (".*osaka-medium.*" . 1.2)
+        (".*courier-bold-.*-mac-roman" . 1.0)
+        (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+        (".*monaco-bold-.*-mac-roman" . 0.9)
+        ("-cdac$" . 1.3))))
+
 ;;; 現在行に色を付ける
 ;(global-hl-line-mode t)
 ;; 色
