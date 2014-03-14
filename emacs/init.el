a183eb3f5acac8c32aff0ab64d8fdc5ae5d53ea3
mtoiya <mtoiya@gmail.com>
2014-03-14 11:09:29 +0900

    emacs/init.elを修正した。 Macでのメタキーを変更（コメントアウトしていたのを戻した）

--- emacs/init.el
+++ emacs/init.el
@@ -466,9 +466,9 @@
   (setq shell-file-name "/bin/zsh"))
 
 ;;; Macのメタキーの設定
-;(when ns-p
-;  (setq ns-command-modifier (quote meta))
-;  (setq ns-alternate-modifier (quote super)))
+(when ns-p
+  (setq ns-command-modifier (quote meta))
+  (setq ns-alternate-modifier (quote super)))
 
 ;;; サーバーモードをスタートします
 (server-start)
