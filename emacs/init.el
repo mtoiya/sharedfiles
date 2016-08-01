2e1276481344801acd3732aa24a1fe44d7d6dfcc
toiya <toiya@tse.co.jp>
2016-08-01 09:59:32 +0900

    emacsの初期設定を修正 Javaモードのindent-tabs-modeをnil（タブの代わりにスペースを使用）に変更

--- emacs/init.el
+++ emacs/init.el
@@ -81,7 +81,7 @@
 ;; Javaモードのタブ・インデント設定
 (add-hook 'java-mode-hook
           '(lambda ()
-             (setq indent-tabs-mode t)
+             (setq indent-tabs-mode nil)
              (setq c-basic-offset 4)
              ))
 
